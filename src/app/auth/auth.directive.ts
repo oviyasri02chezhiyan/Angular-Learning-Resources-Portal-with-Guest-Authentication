974a0c30e51f54eb10f2d2f06ba23de0a18ea533
Oviyasri Elanchezhiyan <oviyasri02chezhiyan@gmail.com>
2025-12-23 21:37:38 +0530

    Add files via upload

---
+++ src/app/auth/auth.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Permission } from './auth.model';
+
+@Directive({
+  selector: '[appAuth]',
+  standalone: true
+})
+export class AuthDirective {
+  userType=input.required<Permission>({alias:'appAuth'});
+  private authService=inject(AuthService);
+  private templateref=inject(TemplateRef);
+  private viewContainerRef=inject(ViewContainerRef)
+  constructor() {
+    effect(()=>{
+      if(this.authService.activePermission() === this.userType()){
+        this.viewContainerRef.createEmbeddedView(this.templateref);
+      }else{
+       this.viewContainerRef.clear();
+      }
+    });
+   }
+
+}
